8e08592d188bc3546123fabd8143fd4e3e037835
charles K <kcharles52@gmail.com>
2018-07-16 11:21:39 +0300

    create function for diary entries tab

---
+++ UI/js/main.js
@@ -0,0 +1,37 @@
+var login = document.getElementById('login');
+var register=document.getElementById('signup');
+function switchTab(a){
+    switch (a) {
+        case login:
+        login.style.display='block';
+        register.style.display='none';
+            break;
+
+        case signup:
+        register.style.display = 'block';
+        login.style.display = 'none';
+            break;
+
+    }
+}
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
