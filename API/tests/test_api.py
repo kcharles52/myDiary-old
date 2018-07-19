7db294a67d41c815ed522bc7660540c5adcb1bda
charles K <kcharles52@gmail.com>
2018-07-19 14:40:00 +0300

    Add test methonds for user login

--- API/tests/test_api.py
+++ API/tests/test_api.py
@@ -32,10 +32,17 @@ class ApiTestCase(unittest.TestCase):
     def test_user_login(self):
         response = self.test_client.post('/login', data=json.dumps(self.user_login_data), content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Welcome Kato. You are logged in",str(response.data))
+        self.assertIn("Welcome. You are logged in",str(response.data))
 
+    def test_login_without_password(self):
+        response = self.test_client.post('/login', data=json.dumps({"email":"kato@gmail.com", "password":" "}), content_type = 'application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('password  is required',str(response.data))
 
-
+    def test_login_without_email(self):
+        response = self.test_client.post('/login', data=json.dumps({"email":" ", "password":"123456"}), content_type = 'application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email is required',str(response.data))
 
 if __name__=='__main__':
     unittest.main()
